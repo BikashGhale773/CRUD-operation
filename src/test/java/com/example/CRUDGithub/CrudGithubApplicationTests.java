d4e7e71c62e4661a2dcd82b08c73dbe14354f545
BikashGhale773 <ghalebikash445@gmail.com>
2023-03-03 14:32:06 -0600

    Initial version

---
+++ src/test/java/com/example/CRUDGithub/CrudGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
