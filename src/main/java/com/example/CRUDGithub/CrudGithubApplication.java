1277d1c7ce44bd7aef3e9fb66dda9a43227bb119
BikashGhale773 <ghalebikash445@gmail.com>
2023-03-04 11:50:11 -0600

    complete crud operation

--- src/main/java/com/example/CRUDGithub/CrudGithubApplication.java
+++ src/main/java/com/example/CRUDGithub/CrudGithubApplication.java
@@ -9,8 +9,7 @@ public class CrudGithubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudGithubApplication.class, args);
 		System.out.println("Hello World");
-		System.out.println("Changes made for github");
-		System.out.println("Controller package is created");
+
 	}
 
 }
--- src/main/java/com/example/CRUDGithub/service/UserService.java
+++ src/main/java/com/example/CRUDGithub/service/UserService.java
@@ -1,4 +1,17 @@
 package com.example.CRUDGithub.service;
 
-public class UserService {
+import com.example.CRUDGithub.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> findAllUsers();
+
+    public User findById(Long id);
+    public User postUser(User user);
+
+    public void deleteUser(Long id);
+
+    public User updateUser(User user);
 }
